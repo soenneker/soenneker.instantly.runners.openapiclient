22e4ac744fe9d681c0fcb8cecf58bf3a6ed87958
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-23 11:36:19 -0500

    lifetime issues

--- src/Soenneker.Instantly.Runners.OpenApiClient/Startup.cs
+++ src/Soenneker.Instantly.Runners.OpenApiClient/Startup.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
 using Soenneker.Kiota.Util.Registrars;
 using Soenneker.Managers.Runners.Registrars;
 using Soenneker.Instantly.Runners.OpenApiClient.Utils;
@@ -21,10 +21,10 @@ public static class Startup
     public static IServiceCollection SetupIoC(this IServiceCollection services)
     {
         services.AddHostedService<ConsoleHostedService>()
-                .AddScoped<IFileOperationsUtil, FileOperationsUtil>()
-                .AddRunnersManagerAsScoped()
-                .AddCloudflareDownloaderAsScoped()
-                .AddKiotaUtilAsScoped();
+                .AddSingleton<IFileOperationsUtil, FileOperationsUtil>()
+                .AddRunnersManagerAsSingleton()
+                .AddCloudflareDownloaderAsSingleton()
+                .AddKiotaUtilAsSingleton();
 
         return services;
     }
